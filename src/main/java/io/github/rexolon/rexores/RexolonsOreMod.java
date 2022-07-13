25a34b588fde4cb173eabfdc7966989a87e1a9a1
rexolon <sarurtz1@gmail.com>
2022-07-13 23:16:03 +0200

    dont ask

--- src/main/java/io/github/rexolon/rexores/RexolonsOreMod.java
+++ src/main/java/io/github/rexolon/rexores/RexolonsOreMod.java
@@ -1,26 +1,12 @@
 package io.github.rexolon.rexores;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
-
 @Mod(RexolonsOreMod.MOD_ID)
 public class RexolonsOreMod {
-    private static final Logger LOGGER = LogUtils.getLogger();
     public static final String MOD_ID = "rexores";
 
     public RexolonsOreMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
