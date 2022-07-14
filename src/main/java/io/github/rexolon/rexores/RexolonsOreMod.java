7231bdfa5fb6e2935b3130b78e63e64ab0672b60
rexolon <sarurtz1@gmail.com>
2022-07-14 18:47:51 +0200

    reset the mod to an no file stage

--- src/main/java/io/github/rexolon/rexores/RexolonsOreMod.java
+++ src/main/java/io/github/rexolon/rexores/RexolonsOreMod.java
@@ -1,11 +1,5 @@
 package io.github.rexolon.rexores;
 
-import io.github.rexolon.rexores.init.BlockInit;
-import io.github.rexolon.rexores.init.ItemInit;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -15,19 +9,8 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class RexolonsOreMod {
     public static final String MOD_ID = "rexores";
 
-    public static final CreativeModeTab REXORES_TAB = new CreativeModeTab(MOD_ID) {
-        @Override
-        @OnlyIn(Dist.CLIENT)
-        public ItemStack makeIcon() {
-            return new ItemStack(ItemInit.LEAD_CHUNK.get());
-        }
-    };
-
     public RexolonsOreMod() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ItemInit.ITEMS.register(bus);
-        BlockInit.BLOCKS.register(bus);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
